212d3bbe5ff466d90355997930819c88a9f7b5a7
Gilaaang <129238448+Gilaaang@users.noreply.github.com>
2024-12-28 15:52:37 +0700

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,6 @@
+public class Bus extends Kendaraan {
+    public Bus(String idkendaraan, int kapasitas, double hargaTiket, boolean statusOperasional, String rute) {
+        super(idkendaraan, "Bus", 30, 50.000,true, rute);
+    }
+
+}
